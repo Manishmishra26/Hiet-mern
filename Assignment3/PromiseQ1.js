23e5a0ce8262f115621940cb680b91fdce1b2673
Manishmishra <mishramanishgkp007@gmail.com>
2025-10-16 11:29:57 +0530

    first commit

---
+++ Assignment3/PromiseQ1.js
@@ -0,0 +1,9 @@
+console.log("Task Start")
+const myPromise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Task Completed")
+    }, 2000)
+})
+.then((result)=>{
+    console.log(result)
+})
